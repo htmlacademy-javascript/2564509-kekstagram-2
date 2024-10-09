6a5c8434b80a9f71b7eefd6bc01eaaad45cc312e
androsovichslava <androsovichslava@mail.ru>
2024-10-09 19:35:26 +0300

    четвертый

--- js/functions.js
+++ js/functions.js
@@ -9,5 +9,9 @@ function checkFunction(str, length) {
 }
 
 function isPolindrom (str) {
+<<<<<<< HEAD
   return str === str.split('').reverse().join('');
+=======
+  return str.toUpperCase() === str.split('').reverse().join('').toUpperCase();
+>>>>>>> ef0d410ca7f7dee334aa847ea99fd5a43d894d46
 }
